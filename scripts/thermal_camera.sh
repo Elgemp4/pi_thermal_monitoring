b06d594e8de562b63e39d4a659d5a173a8b07fdc
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2024-11-12 18:06:52 +0100

    fixed sh launcher

--- scripts/thermal_camera.sh
+++ scripts/thermal_camera.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Define the path to your virtual environment and your main Python script
-VENV_DIR=/usr/local/bin/pi-therm-monitoring/venv
-MAIN_SCRIPT=/usr/local/bin/pi-therm-monitoring/main.py
+VENV_DIR="/usr/local/bin/pi-therm-monitoring/venv"
+MAIN_SCRIPT="/usr/local/bin/pi-therm-monitoring/main.py"
 
 # Activate the virtual environment
-source $VENV_DIR/bin/activate
+source "$VENV_DIR/bin/activate"
 
 # Run the Python program
 python $MAIN_SCRIPT "$@"
