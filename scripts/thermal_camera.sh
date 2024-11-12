6fab4e0601cae417c301e2fa420760bec8ee30d0
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2024-11-12 20:22:28 +0100

    Various fixed

--- scripts/thermal_camera.sh
+++ scripts/thermal_camera.sh
@@ -3,6 +3,6 @@
 # Define the path to your virtual environment and your main Python script
 VENV_DIR="/usr/local/bin/pi-therm-monitoring/venv"
 MAIN_SCRIPT="/usr/local/bin/pi-therm-monitoring/main.py"
-
+sudo modprobe v4l2loopback video_nr=59 card_label="VirtualCam"
 # Run the Python program
 "$VENV_DIR/bin/python" "$MAIN_SCRIPT"
