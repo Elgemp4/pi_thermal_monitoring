ed295241ba8598ef487e4fec3e63568a98327fc9
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2024-12-18 15:54:49 +0100

    Start of refactoring

--- scripts/main.py
+++ scripts/main.py
@@ -11,37 +11,31 @@ from temperature import Zone
 import cv2
 import pyvirtualcam
 import numpy as np
-import argparse
 import time
 import signal
 import sys
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--device", type=int, default=0, help="Video Device number e.g. 0, use v4l2-ctl --list-devices")
-args = parser.parse_args()
-dev = None	
-if args.device:
-	dev = args.device
-else:
-	while(dev == None):
-		try:
-			dev = find_camera_device()
-		except Exception as e:
-			print("Camera not found, retrying in 5 seconds")
-			time.sleep(5)
+
+dev = find_camera_device()
 camera_path = '/dev/video'+str(dev)
 
-width = 256 
-height = 192 
+camera_width = 256 
+camera_height = 192 
 
 cap = cv2.VideoCapture(camera_path, cv2.CAP_V4L)
 cap.set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
 print(camera_path)
 
+next_time_to_send = time.time()
+
+time_for_next_alert = 0
+over_limit = 0
+all_camera = Zone("all", "all", 0, 192, 0, 256)
+
 scale = 1 #scale multiplier
-scaled_width = width * scale
-scaled_height = height * scale
+scaled_width = camera_width * scale
+scaled_height = camera_height * scale
 alpha = 1.0 # Contrast control (1.0-3.0)
 colormap_index = 0
 font=cv2.FONT_HERSHEY_SIMPLEX
@@ -76,89 +70,107 @@ colormaps = [('Jet', cv2.COLORMAP_JET),
 			 ('Viridis', cv2.COLORMAP_VIRIDIS),
 			 ('Parula', cv2.COLORMAP_PARULA)]
 
-def apply_color_map(colormap_index, bgr):
+
+def apply_color_map(colormap_index, im_data):
+	bgr = cv2.cvtColor(im_data, cv2.COLOR_YUV2BGR_YUYV)
+	bgr = cv2.convertScaleAbs(bgr, alpha=alpha)#Contrast
+
+	bgr = cv2.resize(bgr, (scaled_width, scaled_height), interpolation=cv2.INTER_CUBIC)#Scale up!
+	if rad>0:
+		bgr = cv2.blur(bgr,(rad,rad))
+
 	colormap_title, colormap = colormaps[colormap_index]
-	heatmap = cv2.applyColorMap(bgr, colormap)
-	return colormap_title, heatmap
+	heatmap_image = cv2.applyColorMap(bgr, colormap)
+	return heatmap_image
 	
 
-with pyvirtualcam.Camera(width, height, 25, fmt=PixelFormat.BGR, print_fps=25) as cam:
-	print(f'Virtual cam started: {cam.device} ({cam.width}x{cam.height} @ {cam.fps}fps)')
+def handle_camera_reconnect():
+	global cap
+	global camera_path
+	global dev
 
-	with SocketManager() as sm:
-		next_time_to_send = time.time()
-		time_for_next_alert = 0
-		over_limit = 0
-		all_camera = Zone("all", "all", 0, 192, 0, 256)
+	if(os.path.exists(camera_path) == False or cap.isOpened() == False):
+		print("Camera disconnected, attempting to reconnect in 5 seconds")
+		cap.release()
+		time.sleep(5)
 		
-		while True:
-			if(sm.listen_firebase() == False): #If the connection is closed,
-				print("Connection closed")
-				break
+		dev = find_camera_device()
+		camera_path = '/dev/video'+str(dev)
 
-			# Capture frame-by-frame
-			ret, frame = cap.read()
+		cap.open(camera_path, cv2.CAP_V4L)
+		cap.set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
 
-			if not ret:
-				if(os.path.exists(camera_path) == False or cap.isOpened() == False):
-					print("Camera disconnected, attempting to reconnect in 5 seconds")
-					cap.release()
-					time.sleep(5)
-					cap.open(camera_path, cv2.CAP_V4L)
-					cap.set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
+def handle_stream_state(stream_until : time) -> None:
+	if(stream_until > time.time() and not is_streaming()):
+		start_stream(sm.stream_url)
+	elif(stream_until < time.time() and is_streaming()):
+		stop_stream()
+
+def send_temperature_data(sm : SocketManager, th_data : list) -> None:
+	global next_time_to_send
+
+	if(sm.measure_each != -1 and next_time_to_send < time.time()):
+		data = {}
+
+		for zone in sm.zone_list:
+			zone.set_th_data(th_data)
+			(x,y,temp) = zone.find_highest()
+			data[zone.id] = temp
+	
+		next_time_to_send = time.time() + sm.measure_each
+		sm.send_temperature_data(data)
+
+def handle_alerts(sm : SocketManager, th_data : list) -> None:
+	global over_limit
+	global time_for_next_alert
+	global all_camera
+
+	all_camera.set_th_data(th_data)
+
+	(x,y,temp) = all_camera.find_highest()
+
+	if(temp >= sm.max_temp):
+		over_limit += 1
+	else:
+		over_limit = -1
+
+	if(over_limit >= 100):
+		if(time_for_next_alert > time.time()):
+			sm.send_alert()
+			time_for_next_alert = time.time() + 10
+	
+def parse_frame(frame):
+	im_data,raw_th_data = np.array_split(frame, 2)
+	th_data = convertRawToCelcius(raw_th_data)
+	heatmap_image = apply_color_map(colormap_index, im_data)
+	return heatmap_image, th_data
+
+try:
+	with pyvirtualcam.Camera(camera_width, camera_height, 25, fmt=PixelFormat.BGR, print_fps=25) as cam:
+		print(f'Virtual cam started: {cam.device} ({cam.width}x{cam.height} @ {cam.fps}fps)')
+		with SocketManager() as sm:
+			while True:
+				if(sm.listen_firebase() == False): #If the connection is closed,
+					print("Connection closed")
+					break #TODO: Reconnect
+
+				frame_acquired, frame = cap.read()
+
+				if not frame_acquired:
+					handle_camera_reconnect()
 					continue
-				continue
 
-			im_data,raw_th_data = np.array_split(frame, 2)
-			th_data = convertRawToCelcius(raw_th_data)
+				try:
+					heatmap_image, th_data = parse_frame(frame)
 
-			# Convert the real image to RGB
-			bgr = cv2.cvtColor(im_data, cv2.COLOR_YUV2BGR_YUYV)
-			#Contrast
-			bgr = cv2.convertScaleAbs(bgr, alpha=alpha)#Contrast
+					handle_stream_state(sm.stream_until)
 
-			#bicubic interpolate, upscale and blur
-			bgr = cv2.resize(bgr, (scaled_width, scaled_height), interpolation=cv2.INTER_CUBIC)#Scale up!
-			if rad>0:
-				bgr = cv2.blur(bgr,(rad,rad))
+					handle_alerts(sm, th_data)
+					
+					send_temperature_data(sm, th_data)
 
-
-			cmapText, image = apply_color_map(colormap_index, bgr)
-
-			if(sm.stream_until > time.time() and not is_streaming()):
-				start_stream(sm.stream_url)
-			elif(sm.stream_until < time.time() and is_streaming()):
-				stop_stream()
-
-
-			all_camera.set_th_data(th_data)
-
-			(x,y,temp) = all_camera.find_highest()
-
-			if(temp >= sm.max_temp):
-				over_limit += 1
-			else:
-				over_limit = -1
-
-
-			if(over_limit >= 100):
-				if(time_for_next_alert > time.time()):
-					sm.send_alert()
-					time_for_next_alert = time.time() + 10 #10s before next alert
-			
-			if(sm.measure_each != -1 and next_time_to_send < time.time()):
-				data = {}
-				print("Zones : " + str(sm.zone_list))
-				print("Max temp : " + str(sm.max_temp))
-				for all_camera in sm.zone_list:
-					all_camera.set_th_data(th_data)
-					(x,y,temp) = all_camera.find_highest()
-			
-				next_time_to_send = time.time() + sm.measure_each
-				sm.send_temperature_data(data)
-
-			cam.send(image)
-				
-	cap.release()
-
-
+					cam.send(heatmap_image)
+				except Exception as e:
+					print(e)
+finally:
+	cleanly_close_program(None, None)
--- scripts/system_utils.py
+++ scripts/system_utils.py
@@ -1,6 +1,7 @@
+import time
 from linuxpy.video.device import Device, BufferType
 
-def find_camera_device():
+def _find_camera_device():
 	for dev_i in range(64):
 		try:
 			with Device.from_id(dev_i) as device:
@@ -16,3 +17,15 @@ def find_camera_device():
 			pass
 
 	raise Exception("No camera device found")
+
+def find_camera_device():
+	dev = None	
+
+	while(dev == None):
+		try:
+			dev = _find_camera_device()
+		except Exception as e:
+			print("Camera not found, retrying in 5 seconds")
+			time.sleep(5)
+
+	return dev
