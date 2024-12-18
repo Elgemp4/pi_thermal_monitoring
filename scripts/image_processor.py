cb2f1f2787c03b0b1654728ba9b34301617d17a7
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2024-12-18 16:03:22 +0100

    Refactored image processing

---
+++ scripts/image_processor.py
@@ -0,0 +1,43 @@
+import cv2
+
+
+class ImageProcessor:
+    camera_width = 256 
+    camera_height = 192 
+
+    scale = 1 #scale multiplier
+    alpha = 1.0 # Contrast control (1.0-3.0)
+    colormap_index = 0
+    font=cv2.FONT_HERSHEY_SIMPLEX
+    dispFullscreen = False
+    rad = 0 #blur radius
+    threshold = 2
+
+    colormaps = [('Jet', cv2.COLORMAP_JET),
+                ('Hot', cv2.COLORMAP_HOT),
+                ('Magma', cv2.COLORMAP_MAGMA),
+                ('Inferno', cv2.COLORMAP_INFERNO),
+                ('Plasma', cv2.COLORMAP_PLASMA),
+                ('Bone', cv2.COLORMAP_BONE),
+                ('Spring', cv2.COLORMAP_SPRING),
+                ('Autumn', cv2.COLORMAP_AUTUMN),
+                ('Viridis', cv2.COLORMAP_VIRIDIS),
+                ('Parula', cv2.COLORMAP_PARULA)]
+    
+    def get_width(self):
+        return self.camera_width * self.scale
+
+    def get_height(self):
+        return self.camera_height * self.scale
+
+    def apply_color_map(self, im_data):
+        bgr = cv2.cvtColor(im_data, cv2.COLOR_YUV2BGR_YUYV)
+        bgr = cv2.convertScaleAbs(bgr, alpha=self.alpha)#Contrast
+
+        bgr = cv2.resize(bgr, (self.get_width(), self.get_height()), interpolation=cv2.INTER_CUBIC)#Scale up!
+        if self.rad>0:
+            bgr = cv2.blur(bgr,(self.rad,self.rad))
+
+        colormap_title, colormap = self.colormaps[self.colormap_index]
+        heatmap_image = cv2.applyColorMap(bgr, colormap)
+        return heatmap_image
--- scripts/main.py
+++ scripts/main.py
@@ -5,6 +5,8 @@ from temperature import convertRawToCelcius
 from system_utils import find_camera_device
 from firebase_socket import SocketManager
 from video_stream import start_stream, stop_stream, is_streaming
+from image_processor import ImageProcessor
+
 
 from temperature import Zone
 
@@ -16,12 +18,8 @@ import signal
 import sys
 import os
 
-
 dev = find_camera_device()
 camera_path = '/dev/video'+str(dev)
-
-camera_width = 256 
-camera_height = 192 
 
 cap = cv2.VideoCapture(camera_path, cv2.CAP_V4L)
 cap.set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
@@ -31,21 +29,8 @@ next_time_to_send = time.time()
 
 time_for_next_alert = 0
 over_limit = 0
-all_camera = Zone("all", "all", 0, 192, 0, 256)
 
-scale = 1 #scale multiplier
-scaled_width = camera_width * scale
-scaled_height = camera_height * scale
-alpha = 1.0 # Contrast control (1.0-3.0)
-colormap_index = 0
-font=cv2.FONT_HERSHEY_SIMPLEX
-dispFullscreen = False
-rad = 0 #blur radius
-threshold = 2
-hud = True
-recording = False
-elapsed = "00:00:00"
-snaptime = "None"
+all_camera = Zone("all", "all", 0, 192, 0, 256)
 
 
 def cleanly_close_program(sig, frame):
@@ -57,31 +42,6 @@ def cleanly_close_program(sig, frame):
 
 signal.signal(signal.SIGINT, cleanly_close_program)
 signal.signal(signal.SIGTERM, cleanly_close_program)
-
-
-colormaps = [('Jet', cv2.COLORMAP_JET),
-			 ('Hot', cv2.COLORMAP_HOT),
-			 ('Magma', cv2.COLORMAP_MAGMA),
-			 ('Inferno', cv2.COLORMAP_INFERNO),
-			 ('Plasma', cv2.COLORMAP_PLASMA),
-			 ('Bone', cv2.COLORMAP_BONE),
-			 ('Spring', cv2.COLORMAP_SPRING),
-			 ('Autumn', cv2.COLORMAP_AUTUMN),
-			 ('Viridis', cv2.COLORMAP_VIRIDIS),
-			 ('Parula', cv2.COLORMAP_PARULA)]
-
-
-def apply_color_map(colormap_index, im_data):
-	bgr = cv2.cvtColor(im_data, cv2.COLOR_YUV2BGR_YUYV)
-	bgr = cv2.convertScaleAbs(bgr, alpha=alpha)#Contrast
-
-	bgr = cv2.resize(bgr, (scaled_width, scaled_height), interpolation=cv2.INTER_CUBIC)#Scale up!
-	if rad>0:
-		bgr = cv2.blur(bgr,(rad,rad))
-
-	colormap_title, colormap = colormaps[colormap_index]
-	heatmap_image = cv2.applyColorMap(bgr, colormap)
-	return heatmap_image
 	
 
 def handle_camera_reconnect():
@@ -139,14 +99,16 @@ def handle_alerts(sm : SocketManager, th_data : list) -> None:
 			sm.send_alert()
 			time_for_next_alert = time.time() + 10
 	
-def parse_frame(frame):
-	im_data,raw_th_data = np.array_split(frame, 2)
+def parse_frame(frame : list, image_processor : ImageProcessor):
+	im_data, raw_th_data = np.array_split(frame, 2)
 	th_data = convertRawToCelcius(raw_th_data)
-	heatmap_image = apply_color_map(colormap_index, im_data)
+	heatmap_image = image_processor.apply_color_map(im_data)
 	return heatmap_image, th_data
 
 try:
-	with pyvirtualcam.Camera(camera_width, camera_height, 25, fmt=PixelFormat.BGR, print_fps=25) as cam:
+	image_processor = ImageProcessor()
+
+	with pyvirtualcam.Camera(image_processor.get_width(), image_processor.get_height(), 25, fmt=PixelFormat.BGR, print_fps=25) as cam:
 		print(f'Virtual cam started: {cam.device} ({cam.width}x{cam.height} @ {cam.fps}fps)')
 		with SocketManager() as sm:
 			while True:
@@ -161,7 +123,7 @@ try:
 					continue
 
 				try:
-					heatmap_image, th_data = parse_frame(frame)
+					heatmap_image, th_data = parse_frame(frame, image_processor)
 
 					handle_stream_state(sm.stream_until)
 
