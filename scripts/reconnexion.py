2b2afc0a45b9b897fb72f1a706a544c004100389
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2024-12-22 17:29:15 +0100

    Changed script for network binding issues

--- scripts/reconnexion.py
+++ scripts/reconnexion.py
@@ -9,6 +9,8 @@ ACCESS_POINT_INTERFACE = "wlan0"
 WIFI_INTERFACE = "wlan1"
 AP_PORT = 80
 
+ap_thread : threading.Thread = None
+
 # Minimalist HTML template for the web page
 HTML_TEMPLATE = """
 <!DOCTYPE html>
@@ -46,6 +48,7 @@ def connect_to_wifi(ssid, password):
         subprocess.run(["nmcli", "dev", "wifi", "connect", ssid, "password", password, "ifname", WIFI_INTERFACE], check=True)
         print(f"Connected to Wi-Fi network '{ssid}'. Stopping access point...")
         stop_access_point()
+        ap_thread.join()
     except subprocess.CalledProcessError:
         print(f"Failed to connect to Wi-Fi network '{ssid}'.")
 
@@ -68,6 +71,12 @@ def monitor_wifi_connection():
         if not is_connected(WIFI_INTERFACE) and not is_connected(ACCESS_POINT_INTERFACE):
             print("Wi-Fi disconnected. Starting access point...")
             start_access_point()
+            time.sleep(5)
+            ap_thread = threading.Thread(target=lambda: app.run(host="10.42.0.1", port=AP_PORT))
+            ap_thread.start()
+        elif is_connected(WIFI_INTERFACE) and is_connected(ACCESS_POINT_INTERFACE):
+            stop_access_point()
+            ap_thread.join()
         time.sleep(10)  # Check every 10 seconds
 
 @app.route("/", methods=["GET", "POST"])
@@ -88,5 +97,5 @@ if __name__ == "__main__":
     time.sleep(60) #Sleep for 60 seconds to give time to the network manager to start and connect to the network
     # Start the Flask server
     print(f"Starting Flask server")
-    threading.Thread(target=lambda: app.run(host="10.42.0.1", port=AP_PORT)).start()
+    
     monitor_wifi_connection()
